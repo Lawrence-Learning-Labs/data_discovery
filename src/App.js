faf18998b6785978016e930956c9e65f9486aa19
Timothy Hurt <timothyjhurt@gmail.com>
2020-12-03 14:02:41 -0800

    testing it

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 
 import React from 'react';
 import Navbar from './components/Navbar';
-import {BrowserRouter as Router, Switch, Route } from
+import {BrowserRouter, Router, Switch, Route} from
 'react-router-dom';
 import './App.css';
 import Home from './components/pages/Home'
@@ -12,10 +12,12 @@ function App() {
   <>
   <Router>
     <Navbar />
-    <Switch>
-      <Route path = '/' exact component = {Home}/>
-      <Route path = '/Experience1' component = {Experience1}/>
-      <Route path = '/Experience2' component = {Experience2}/>
+      <Switch>
+        <BrowserRouter basename={process.env.PUBLIC_URL}>
+          <Route path = '/' exact component = {Home}/>
+          <Route path = '/Experience1' component = {Experience1}/>
+          <Route path = '/Experience2' component = {Experience2}/>
+        </BrowserRouter>
       </Switch>
     </Router>
     </>
